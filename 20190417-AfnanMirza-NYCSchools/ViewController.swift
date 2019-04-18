5824f8e0d44aaa35616ff0d01556ecdf402dd97c
Afnan Mirza <afnanmirza1999@gmail.com>
2019-04-17 20:27:13 -0400

    Initial Commit

---
+++ 20190417-AfnanMirza-NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190417-AfnanMirza-NYCSchools
+//
+//  Created by Afnan Mirza on 4/17/19.
+//  Copyright © 2019 Afnan Mirza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
